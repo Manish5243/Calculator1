a188e4b16f4b170acde94fcd0eb0a69079c1ba93
Manish kumar <manishkumargolu90@gmail.com>
2018-05-25 14:47:52 +0530

    Calculator

---
+++ Calculator.js
@@ -0,0 +1,34 @@
+function btnplus()
+{
+document.Calculator.display.value +="+";
+document.Calculator.display.style.textAlign="center";
+}
+function btnsub()
+{
+document.Calculator.display.value +="-";
+document.Calculator.display.style.textAlign="center";
+}
+function btnmul()
+{
+document.Calculator.display.value +="*";
+document.Calculator.display.style.textAlign="center";
+}
+function btnmod()
+{
+document.Calculator.display.value +="%";
+document.Calculator.display.style.textAlign="center";
+}
+function btndot()
+{
+document.Calculator.display.value +=".";
+document.Calculator.display.style.textAlign="center";
+}
+function btndiv()
+{
+document.Calculator.display.value +="/";
+document.Calculator.display.style.textAlign="center";
+}
+function btnclr()                
+{ 
+document.Calculator.display.style.textAlign="center";
+}
